ea7d59352e399d4c80f992e7aee755d6a8593cf3
maleneivy <68667843+maleneivy@users.noreply.github.com>
2022-02-26 14:49:33 +0100

    updated js files and css styling

---
+++ js/question-1.js
@@ -0,0 +1,9 @@
+// Question 1
+// Convert the function below to an arrow function:
+
+const remainder = (a, b) => a % b // //the return is implicit med syntax is like that.
+
+const getRemainder = remainder(5, 2);
+
+console.log(getRemainder);
+
--- js/question-2.js
+++ js/question-2.js
@@ -1,13 +1,3 @@
-// Question 1
-// Convert the function below to an arrow function:
-
-const remainder = (a, b) => a % b // //the return is implicit med syntax is like that.
-
-const getRemainder = remainder(5, 2);
-
-console.log(getRemainder);
-
-
 // Question 2 
 // Make a call to the Rawg API.
 
